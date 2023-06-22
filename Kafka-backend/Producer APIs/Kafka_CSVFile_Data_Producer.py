a36f505a6f0452da3bf3e43541bdc10ebfd5bf89
Doshi <Isha.Doshi@sms-group.com>
2023-06-22 12:04:54 -0400

    added producer apis and kafka connect

---
+++ Kafka-backend/Producer APIs/Kafka_CSVFile_Data_Producer.py
@@ -0,0 +1,58 @@
+import os
+import time
+import pandas as pd
+from confluent_kafka import Producer
+import json
+
+# Get user inputs
+kafka_broker = "localhost:9092"
+data_dir = input("Enter the directory path containing the CSV files: ")
+kafka_broker = input("Enter the Kafka broker (format - localhost:9092): ") or kafka_broker
+kafka_topic = input("Enter the Kafka topic: ")
+
+# Create a producer to send data to Kafka
+producer = Producer({
+    'bootstrap.servers': kafka_broker,
+    'enable.idempotence': True,
+    'queue.buffering.max.messages': 10000000,  # Set the desired queue size
+    'queue.buffering.max.ms': 0,
+    'compression.type': 'zstd',  # 'gzip' Or 'snappy', 'lz4', 'zstd'
+    'acks': 'all'  # or '0' or '1' or '-1'/ 'all'
+})
+
+
+def process_file(filepath):
+    # Load the CSV file into a pandas DataFrame
+    df = pd.read_csv(filepath)
+
+    try:
+        for _, row in df.iterrows():
+            # Convert each row to a dictionary
+            row_dict = row.to_dict()
+            
+            # Serialize the row to JSON
+            message_json = json.dumps(row_dict)
+
+            # Send the message to Kafka
+            producer.produce(topic=kafka_topic, value=message_json)
+            
+            time.sleep(0.1)
+    finally:
+        # Flush any outstanding messages
+        producer.flush()
+
+# Get the list of files already processed
+processed_files = set()
+
+while True:
+    # List all CSV files in the directory
+    files = [f for f in os.listdir(data_dir) if f.endswith('.csv')]
+
+    # Process any new files
+    for file in files:
+        if file not in processed_files:
+            process_file(os.path.join(data_dir, file))
+            processed_files.add(file)
+
+    # Wait for a while before checking the directory again
+    time.sleep(10)
---
+++ Kafka-backend/Producer APIs/Kafka_Random_Data_Producer.py
@@ -0,0 +1,49 @@
+import time
+import random
+import json
+from confluent_kafka import Producer
+from datetime import datetime
+
+# Get user inputs
+kafka_broker = "localhost:9092"
+kafka_broker = input("Enter the Kafka broker (format - localhost:9092): ") or kafka_broker
+kafka_topic = input("Enter the Kafka topic: ")
+num_sensors = int(input("Enter the number of sensors to generate data for: "))
+
+# Create a producer to send data to Kafka
+producer = Producer({
+    'bootstrap.servers': kafka_broker,
+    'enable.idempotence': True,
+    'queue.buffering.max.messages': 10000000,  # Set the desired queue size
+    'queue.buffering.max.ms': 500,
+    'compression.type': 'zstd',  # 'gzip' Or 'snappy', 'lz4', 'zstd'
+    'acks': 'all'  # or '0' or '1' or '-1'/ 'all'
+})
+
+while True:
+    try:
+        for i in range(num_sensors):
+            sensor_name = f'sensor_{i}'
+            timestamp = datetime.utcnow().isoformat()
+            value = random.random()  # Generate a random float between 0 and 1
+
+            # Create a message to be sent to Kafka
+            message = {
+                'timestamp': timestamp,
+                'sensor_name': sensor_name,
+                'value': value
+            }
+
+            # Serialize the message to JSON
+            message_json = json.dumps(message)
+
+            # Send the message to Kafka
+            producer.produce(topic=kafka_topic, value=message_json, key=sensor_name)
+            #time.sleep(0.1)
+
+    finally:
+        # Close the producer
+        producer.flush()
+
+    # Wait for a while before generating the data again
+    time.sleep(1)
