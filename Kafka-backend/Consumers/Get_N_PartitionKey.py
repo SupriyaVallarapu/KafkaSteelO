6979f1b9d08c251cfa021cdd6907748f5b39fe4c
Doshi <Isha.Doshi@sms-group.com>
2023-06-30 09:56:30 -0400

    fixed blueprint issues

--- Kafka-backend/Consumers/Get_N_PartitionKey.py
+++ Kafka-backend/Consumers/Get_N_PartitionKey.py
@@ -134,8 +134,8 @@ def consume_messages_n(consumer, topic, n):
 
     return messages
 
-@get_n_partitionkey_blueprint.route('/get/<offset>/<topic>/<group_id>/<int:n>', methods=['GET'])
-@get_n_partitionkey_blueprint.route('/get/<offset>/<topic>/<group_id>/<int:n>', methods=['GET'],defaults={'partition_key': ''})
+# @get_n_partitionkey_blueprint.route('/get/<offset>/<topic>/<group_id>/<int:n>', methods=['GET'])
+@get_n_partitionkey_blueprint.route('/get/<offset>/<topic>/<group_id>/<int:n>', methods=['GET'], defaults={'partition_key': ''})
 @handle_exceptions
 @validate_input
 def get_partition_and_messages(offset, topic, group_id, n):
