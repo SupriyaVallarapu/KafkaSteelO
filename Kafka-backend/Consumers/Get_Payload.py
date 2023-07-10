86e1ea1e8e44fd5a94a514dd22651184724dbb95
Vallarapu <Supriya.Vallarapu@sms-group.com>
2023-07-10 17:11:04 -0400

    changed database apis

--- Kafka-backend/Consumers/Get_Payload.py
+++ Kafka-backend/Consumers/Get_Payload.py
@@ -71,6 +71,67 @@
 # # if __name__ == '__main__':
 # #     app.run(port=3002)
 
+# import json
+# from flask import Blueprint, Flask, Response, abort
+# from confluent_kafka import Consumer, TopicPartition
+
+# get_payload_blueprint = Blueprint('get_payload_blueprint', __name__)
+
+# def consume_messages_all(consumer_config, topic):
+#     consumer = Consumer(consumer_config)
+#     topics = consumer.list_topics().topics
+#     if topic not in topics:
+#         raise Exception("Topic '{}' does not exist".format(topic))
+
+#     # Get the list of partitions for the topic
+#     partitions = consumer.list_topics(topic).topics[topic].partitions.keys()
+#     consumer.assign([TopicPartition(topic, partition) for partition in partitions])
+
+#     no_message_count = 0
+#     max_no_message_count = 100  # 100 * 1.0 seconds = 100 seconds wait in worst case
+
+#     while True:
+#         msg = consumer.poll(1.0)
+#         if msg is None:
+#             no_message_count += 1
+#             if no_message_count > max_no_message_count:
+#                 break  # Leave while loop after max retries
+#             continue
+#         else:
+#             no_message_count = 0
+
+#         if msg.error():
+#             print("Error: {}".format(msg.error()))
+#             continue
+
+#         message = msg.value().decode('utf-8')
+#         payload = json.loads(message)['payload']  # Extract 'payload' from the JSON message
+#         yield json.dumps(payload) + '\n'
+
+#     consumer.close()
+
+# @get_payload_blueprint.route('/get/payload/<offset>/<topic>/<group_id>', methods=['GET'])
+# def get_all_messages(offset, topic, group_id):
+#     # Validate inputs
+#     if not offset or not topic or not group_id:
+#         abort(400, 'Missing required parameters.')
+
+#     offset = offset.lower()
+#     if offset not in ['earliest', 'latest']:
+#         abort(400, 'Invalid offset.')
+
+#     consumer_config = {
+#         'bootstrap.servers': 'kafka1:19092',
+#         'group.id': group_id,
+#         'auto.offset.reset': offset,
+#         'enable.auto.commit': False
+#     }
+
+#     try:
+#         return Response(consume_messages_all(consumer_config, topic), mimetype='application/json')
+#     except Exception as e:
+#         abort(500, 'An error occurred while consuming messages.')
+
 import json
 from flask import Blueprint, Flask, Response, abort
 from confluent_kafka import Consumer, TopicPartition
@@ -83,27 +144,34 @@ def consume_messages_all(consumer_config, topic):
     if topic not in topics:
         raise Exception("Topic '{}' does not exist".format(topic))
 
-    # Get the list of partitions for the topic
     partitions = consumer.list_topics(topic).topics[topic].partitions.keys()
     consumer.assign([TopicPartition(topic, partition) for partition in partitions])
+
+    no_message_count = 0
+    max_no_message_count = 100
 
     while True:
         msg = consumer.poll(1.0)
         if msg is None:
-            break
+            no_message_count += 1
+            if no_message_count > max_no_message_count:
+                break
+            continue
+        else:
+            no_message_count = 0
+
         if msg.error():
             print("Error: {}".format(msg.error()))
             continue
 
         message = msg.value().decode('utf-8')
-        payload = json.loads(message)['payload']  # Extract 'payload' from the JSON message
-        yield json.dumps(payload) + '\n'
+        payload = json.loads(message)['payload']
+        yield f'data: {json.dumps(payload)}\n\n'
 
     consumer.close()
 
 @get_payload_blueprint.route('/get/payload/<offset>/<topic>/<group_id>', methods=['GET'])
 def get_all_messages(offset, topic, group_id):
-    # Validate inputs
     if not offset or not topic or not group_id:
         abort(400, 'Missing required parameters.')
 
@@ -119,7 +187,6 @@ def get_all_messages(offset, topic, group_id):
     }
 
     try:
-        return Response(consume_messages_all(consumer_config, topic), mimetype='application/json')
+        return Response(consume_messages_all(consumer_config, topic), mimetype='text/event-stream')
     except Exception as e:
         abort(500, 'An error occurred while consuming messages.')
-
--- Kafka-backend/Consumers/Get_Schema.py
+++ Kafka-backend/Consumers/Get_Schema.py
@@ -1,8 +1,8 @@
-# import json
-# from flask import Blueprint, Flask, Response
-# from confluent_kafka import Consumer, KafkaException, TopicPartition
+import json
+from flask import Blueprint, Flask, Response
+from confluent_kafka import Consumer, KafkaException, TopicPartition
 
-# get_schema_blueprint = Blueprint('get_schema_blueprint', __name__)
+get_schema_blueprint = Blueprint('get_schema_blueprint', __name__)
 
 # def consume_latest_message(consumer_config, topic):
 #     consumer = Consumer(consumer_config)
@@ -76,66 +76,40 @@
 #         return Response(status=404)
 
 
-import json
-from flask import Blueprint, Flask, Response
-from confluent_kafka import Consumer, KafkaException, TopicPartition
-
-get_schema_blueprint = Blueprint('get_schema_blueprint', __name__)
-
-def consume_latest_message(consumer_config, topic):
+def consume_messages(consumer_config, topic):
     consumer = Consumer(consumer_config)
 
     try:
-        # Get the partitions for the topic
+        # Assign all partitions of the topic to the consumer
         partitions = consumer.list_topics(topic).topics[topic].partitions.keys()
-        num_partitions = len(partitions)
+        consumer.assign([TopicPartition(topic, p) for p in partitions])
 
-        if num_partitions == 1:
-            # If there is only one partition, take the last offset of that partition
-            partition = partitions[0]
-            consumer.assign([TopicPartition(topic, partition)])
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                break
+            if msg.error():
+                print(f"Kafka Error: {msg.error()}")
+                continue
 
-            end_offset = consumer.get_watermark_offsets(TopicPartition(topic, partition))[1]
-            last_offset = end_offset - 1
-
-        elif num_partitions > 1:
-            # If there are multiple partitions, follow the existing logic to find the last offset
-            last_partition = max(partitions)
-            consumer.assign([TopicPartition(topic, last_partition)])
-
-            end_offset = consumer.get_watermark_offsets(TopicPartition(topic, last_partition))[1]
-            last_offset = end_offset - 1
-
-        else:
-            # Handle the case where there are no partitions
-            return None
-
-        latest_message = None
-
-        if last_offset >= 0:
-            # Seek to the last offset and fetch the message
-            consumer.seek(TopicPartition(topic, partition, last_offset))
-            msg = consumer.poll(5.0)
-
-            if msg is not None and not msg.error():
-                message = msg.value().decode('utf-8')
-                fields = json.loads(message)['schema']['fields']
-                extracted_fields = [{'type': field['type'], 'field': field['field']} for field in fields]
-                yield json.dumps(extracted_fields) + '\n'
+            # Decode message and extract schema
+            message = msg.value().decode('utf-8')
+            fields = json.loads(message)['schema']['fields']
+            extracted_fields = [{'type': field['type'], 'field': field['field']} for field in fields]
+            
+            # Yield schema of the current message
+            yield f'data: {json.dumps(extracted_fields)}\n\n'
 
     except KafkaException as ke:
-        # Handle Kafka-related exceptions
         print(f"KafkaException: {ke}")
-
     except Exception as e:
-        # Handle other exceptions
         print(f"Exception: {e}")
-
     finally:
         consumer.close()
 
+
 @get_schema_blueprint.route('/get/schema/<topic>/<group_id>', methods=['GET'])
-def get_latest_message_schema(topic, group_id):
+def get_messages_schema(topic, group_id):
     consumer_config = {
         'bootstrap.servers': 'kafka1:19092',
         'group.id': group_id,
@@ -144,7 +118,6 @@ def get_latest_message_schema(topic, group_id):
     }
 
     try:
-        return Response(consume_latest_message(consumer_config, topic), mimetype='application/json', headers={'Content-Type': 'application/json'})
+        return Response(consume_messages(consumer_config, topic), mimetype='text/event-stream')
     except Exception as e:
-        return Response(status=500)
-
+        return {"error": str(e)}, 500
